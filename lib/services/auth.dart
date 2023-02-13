3bea96c9364194a2944142f4f15346aeac8ccef1
Katherine Bellman <katherine.bellman@dcmail.ca>
2023-02-13 17:15:45 -0500

    Began auth class for firebase-auth

---
+++ lib/services/auth.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in  as anon
+
+  //sign in  with email + pass
+
+  //register with email + pass
+
+  //sign out
+}
